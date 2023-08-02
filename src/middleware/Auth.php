12cc84d4dbaf671ac56d27a1559a0229a81abdc2
JohnZCastillo <johnzunigacastillo@gmail.com>
2023-08-02 16:53:37 +0800

    fix auth message

--- src/middleware/Auth.php
+++ src/middleware/Auth.php
@@ -6,16 +6,24 @@ use App\lib\Login;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Slim\Flash\Messages;
 use Slim\Psr7\Response;
+use UMA\DIC\Container;
 
 class Auth{
 
-    public function __invoke(Request $request, RequestHandler $handler): ResponseInterface {            
-        
+    private  Messages $flashMessge;
+    public function __construct(Container  $container)
+    {
+        $this->flashMessge =  $container->get(Messages::class);
+    }
+    public function __invoke(Request $request, RequestHandler $handler): ResponseInterface {
+
         if (Login::isLogin()) {
             $response = $handler->handle($request);
             return $response;
         } else {
+            $this->flashMessge->addMessage('AuthFailedMessage','You Must login First');
             $response = new Response();
             return $response->withHeader('Location', '/login')->withStatus(302);
         }
