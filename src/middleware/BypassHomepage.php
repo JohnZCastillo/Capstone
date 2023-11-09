aec8cccae954f7fff33bc974dd7163ff9832aee0
johnzcastillo <johnzunigacastillo@gmail.com>
2023-11-09 11:54:32 +0800

    fix bypass homepage error

--- src/middleware/BypassHomepage.php
+++ src/middleware/BypassHomepage.php
@@ -6,30 +6,34 @@ use App\lib\Login;
 use App\lib\Redirector;
 use App\model\UserModel;
 use App\service\UserService;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
-use Slim\Flash\Messages;
 use Slim\Psr7\Response;
 use UMA\DIC\Container;
 
-class BypassHomepage{
-
+class BypassHomepage
+{
 
     private UserModel $user;
+    private Container $container;
 
-    public function __construct(Container  $container)
+    public function __construct(Container $container)
     {
-        $loginId = Login::getLogin();
-        $userService = $container->get(UserService::class);
-        $this->user= $userService->findById($loginId);
+        $this->container = $container;
+
     }
 
-    public function __invoke(Request $request, RequestHandler $handler): ResponseInterface {
+    public function __invoke(Request $request, RequestHandler $handler): ResponseInterface
+    {
 
         if (!Login::isLogin()) {
             return $handler->handle($request);
         } else {
+            $loginId = Login::getLogin();
+
+            $userService = $this->get(UserService::class);
+            $this->user = $userService->findById($loginId);
             $location = Redirector::redirectToHome($this->user->getPrivileges());
             $response = new Response();
             return $response->withHeader('Location', $location)->withStatus(302);
