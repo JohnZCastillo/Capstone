d893958a8aac4d7b604fb2553acf3665a23308d6
johnzcastillo <johnzunigacastillo@gmail.com>
2023-12-07 12:28:23 +0800

    update cli config

--- cli-config.php
+++ cli-config.php
@@ -2,9 +2,20 @@
 
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
-use Slim\Container;
+use UMA\DIC\Container;
+use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
+
+require __DIR__ . '/vendor/autoload.php';
+
 
 /** @var Container $container */
 $container = require_once __DIR__ . '/bootstrap.php';
 
-return ConsoleRunner::createHelperSet($container->get(EntityManager::class));
+$entityManager = $container->get(EntityManager::class);
+
+$commands = [];
+
+ConsoleRunner::run(
+    new SingleManagerProvider($entityManager),
+    $commands
+);
