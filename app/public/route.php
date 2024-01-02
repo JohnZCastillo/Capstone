7bb698ca2735055faaef09669ca8ce4af57a0c03
John <johnzunigacastillo@gmail.com>
2024-01-02 21:40:37 +0800

    remove pdf

--- app/public/route.php
+++ app/public/route.php
@@ -55,9 +55,6 @@ return function (App $app) use ($twig) {
         \App\controller\auth\ViewLandingPage::class
     );
 
-    $app->get('/pdf',
-        \App\controller\pdf\DownloadPdf::class
-    );
 
     $app->post('/lot',
         \App\controller\api\area\FindLot::class
