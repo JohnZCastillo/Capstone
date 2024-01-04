34b695ca8d9ec625d642c4c4cd1b121c68597a6e
John <johnzunigacastillo@gmail.com>
2024-01-05 00:59:56 +0800

    add documentation

--- src/lib/ReferenceExtractor.php
+++ src/lib/ReferenceExtractor.php
@@ -5,17 +5,26 @@ namespace App\lib;
 class ReferenceExtractor
 {
 
-    public  static function extractReference(array $imagePath): array{
+    /**
+     * Extracts GCASH References using OCR from an array of images.
+     * If reference is not found then a null value is placed.
+     *
+     * @param array $imagePath Array containing GCASH Receipts images.
+     * @return array References obtained from the receipts.
+     */
+
+    public static function extractReference(array $imagePath, string $pattern = '/ref no\. (\d{4}\s\d{3}\s\d{6})/i'): array
+    {
 
         $references = [];
 
-        foreach ($imagePath['tmp_name'] as $index => $tmp_name){
+        foreach ($imagePath['tmp_name'] as $index => $tmp_name) {
 
             $currentImage = $imagePath['tmp_name'][$index];
+
             $ocrStringResult = Ocr::getText($currentImage);
             $ocrStringResult = strtolower($ocrStringResult);
 
-            $pattern = '/ref no\. (\d{4}\s\d{3}\s\d{6})/i';
             preg_match($pattern, $ocrStringResult, $matches);
 
             if (isset($matches[1])) {
