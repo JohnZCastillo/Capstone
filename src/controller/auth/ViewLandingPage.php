64ebc20667bb55d96d644c543717c936911d833b
John <johnzunigacastillo@gmail.com>
2024-01-05 11:43:07 +0800

    update landingpage

--- src/controller/auth/ViewLandingPage.php
+++ src/controller/auth/ViewLandingPage.php
@@ -13,6 +13,41 @@ class ViewLandingPage extends AdminAction
      */
     protected function action(): Response
     {
-       return  $this->view('homepage.html',[]);
+        $overview = $this->overviewService->getOverview();
+
+        $staffs = $this->overviewService->getAllStaff();
+
+        $features = $this->overviewService->getAllFeatures();
+
+        $orgStaff = [];
+
+        foreach ($staffs as $staff) {
+
+            $img = $staff->getImg();
+            $name = $staff->getName();
+            $position = $staff->getPosition();
+
+            $superior = $staff->getSuperior();
+            $superiorName = '';
+
+            if (isset($superior)) {
+                $superiorName = $superior->getName();
+            }
+
+            $orgStaff[] = [
+                'name' => $staff->getName(),
+                'position' => $position,
+                'img' => $img,
+                'superior' => $superiorName,
+            ];
+
+        }
+
+        return $this->view('homepage.html',[
+            "overview" => $overview,
+            "staffs" => $staffs,
+            "org" => $orgStaff,
+            "features" => $features,
+        ]);
     }
 }
