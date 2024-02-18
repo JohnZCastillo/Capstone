3ca6df0517541811b3b0dcc9c80cf563f255b23a
John <johnzunigacastillo@gmail.com>
2024-02-19 00:59:22 +0800

    add transaction

--- src/controller/admin/payments/ManualPayment.php
+++ src/controller/admin/payments/ManualPayment.php
@@ -4,26 +4,13 @@ declare(strict_types=1);
 
 namespace App\controller\admin\payments;
 
-use App\controller\admin\AdminAction;
 use App\exception\AlreadyPaidException;
-use App\exception\ContentLock;
 use App\exception\date\InvalidDateRange;
-use App\exception\image\ImageNotGcashReceiptException;
-use App\exception\image\UnsupportedImageException;
-use App\exception\NotUniqueReferenceException;
 use App\exception\payment\InvalidPaymentAmount;
-use App\exception\payment\InvalidReference;
-use App\exception\payment\TransactionNotFound;
-use App\lib\GCashReceiptValidator;
-use App\lib\Image;
-use App\lib\ReferenceExtractor;
 use App\lib\Time;
 use App\model\enum\LogsTag;
-use App\model\TransactionModel;
-use App\model\UserModel;
 use Exception;
 use Psr\Http\Message\ResponseInterface as Response;
-use Respect\Validation\Validator as v;
 
 class ManualPayment extends Payment
 {
@@ -58,13 +45,14 @@ class ManualPayment extends Payment
                 throw new InvalidPaymentAmount("Payment must be equal to $amountToPay");
             }
 
-
             $this->approvedTransaction($transaction);
             $this->generateTransactionReceipt($transaction);
 
             $actionMessage = 'Manual payment wit id of '. $transaction->getId(). ' was created';
 
             $this->addActionLog($actionMessage, LogsTag::manualPayment());
+
+            $this->setupIncome($transaction);
 
         } catch (AlreadyPaidException $paidException) {
             $this->addErrorMessage($paidException->getMessage());
@@ -75,6 +63,7 @@ class ManualPayment extends Payment
         }  catch (Exception $e) {
             $this->addErrorMessage('Internal Error, please check logs');
         }
+
         return $this->redirect('/admin/payments');
     }
 
