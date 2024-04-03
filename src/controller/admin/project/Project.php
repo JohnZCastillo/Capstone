a92cf50f81a4a6a0f9d01276f439097c8af9a963
John <johnzunigacastillo@gmail.com>
2024-04-03 16:07:54 +0800

    init controller

---
+++ src/controller/admin/project/Project.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\controller\admin\project;
+
+use App\controller\admin\AdminAction;
+use App\exception\InvalidInput;
+use App\lib\Time;
+use App\model\enum\LogsTag;
+use Exception;
+use Psr\Http\Message\ResponseInterface as Response;
+use Respect\Validation\Validator as v;
+
+class Project extends AdminAction
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function action(): Response
+    {
+        try {
+
+            return $this->respondWithData(['message' => 'hello world']);
+        } catch (Exception $exception) {
+            return $this->respondWithData(['message' => $exception->getMessage()],500);
+        }
+
+    }
+}
